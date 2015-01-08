bb7935d154087edc15a536bb55aebf7f0e464e95
dave <oblivion.davecave@gmail.com>
2015-01-08 19:46:41 -0300

    second commit

--- avrduda.sh
+++ avrduda.sh
@@ -1,7 +1,10 @@
 #/bin/bash
 #@uthor: dave
 
+echo Compiling code...
 avr-gcc -mmcu=atmega8 -Os $1 -o obj.o
 avr-objcopy -j .text -j .data -O ihex  obj.o hx.hex
+echo Uploading code...
 sudo avrdude -c usbasp -p m8 -P usb -U flash:w:hx.hex
 rm obj.o hx.hex
+echo Finished.
